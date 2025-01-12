f20554d608a710ccfe1327d9e59551cb2396f76c
Tanmay-Mirgal <tanmaymirgal26@gmail.com>
2025-01-12 13:50:52 +0530

    stats responsive

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -31,11 +31,10 @@ const ProfilePage = () => {
   return (
     <div className="min-h-screen bg-white">
       <Header user={parsedUser} />
-      <div className="max-w-xl px-4 py-2 -mt-9 ml-[calc(100vw-30vw)] md:ml-[30vh]">
-        <div className="flex justify-evenly">
-        <div
+      <div className="max-w-xl px-4 py-2 -mt-9 ml-auto mr-auto md:ml-[30vh]">
+        <div className="flex justify-around">
+          <div
             className="text-center cursor-pointer"
-           
           >
             <h2 className="text-lg font-semibold">{parsedUser.posts?.length || 0}</h2>
             <p className="text-xs text-gray-500">Posts</p>
