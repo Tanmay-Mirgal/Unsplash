64ac8d199d06dc01490edc227a6ff01f5aaccde5
Tanmay-Mirgal <tanmaymirgal26@gmail.com>
2025-01-12 12:09:37 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use(fileUpload({
     createParentPath: true,
 }));
 
-// app.use(express.static(path.join(__dirname,'frontend/dist')));
+app.use(express.static(path.join(__dirname,'frontend/dist')));
 
 //Routes
 app.use('/api/auth',authRoutes);
@@ -67,9 +67,9 @@ setTimeout(async () => {
     }
 }, 5000);
 
-// app.use('*',(req,res)=>{
-//     res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
-// })
+app.use('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 
 app.listen(PORT,()=>{
     connectDB();
