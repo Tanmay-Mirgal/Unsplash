77f2c515516052199d025b885fa8a8650b4e20f2
Tanmay Mirgal <138283976+Tanmay-Mirgal@users.noreply.github.com>
2025-03-09 19:46:57 +0530

    revert the code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,22 @@
-import { useEffect, lazy, Suspense } from "react";
+import { useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "../src/pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import ProfilePage from "./pages/Profile/Profile";
+import UpdateProfile from "./pages/Profile/UpdateProfile";
+import Error from "./pages/Error/Error";
 import { useUserStore } from "./Store/useUserStore";
 import { Toaster } from "react-hot-toast";
 import { Loader2 } from "lucide-react";
 import Navbar from "./pages/Home/components/Navbar";
+import Footer from "./pages/Home/components/Footer";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
-// Lazy load pages
-const Home = lazy(() => import("../src/pages/Home/Home"));
-const Login = lazy(() => import("./pages/Login/Login"));
-const Signup = lazy(() => import("./pages/Signup/Signup"));
-const ProfilePage = lazy(() => import("./pages/Profile/Profile"));
-const UpdateProfile = lazy(() => import("./pages/Profile/UpdateProfile"));
-const Error = lazy(() => import("./pages/Error/Error"));
-
 function App() {
-  const { getProfile, isLoading } = useUserStore((state) => ({
-    getProfile: state.getProfile,
-    isLoading: state.isLoading,
-  }));
-  const token = localStorage.getItem("accessToken, refreshToken");
+  const { getProfile, isLoading } = useUserStore();
+  const token = localStorage.getItem("token");
   const isAuthenticated = !!token;
 
   useEffect(() => {
@@ -40,29 +36,28 @@ function App() {
   return (
     <>
       <Navbar hidden={window.location.pathname === "/login" || window.location.pathname === "/signup"} />
-      <Suspense fallback={<div className="flex justify-center items-center h-screen"><Loader2 className="animate-spin size-16 text-zinc-600" /></div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/login"
-            element={!isAuthenticated ? <Login /> : <Navigate to="/profile" replace />}
-          />
-          <Route
-            path="/signup"
-            element={!isAuthenticated ? <Signup /> : <Navigate to="/profile" replace />}
-          />
-          <Route
-            path="/profile"
-            element={isAuthenticated ? <ProfilePage /> : <Navigate to="/login" replace />}
-          />
-          <Route
-            path="/update-profile"
-            element={isAuthenticated ? <UpdateProfile /> : <Navigate to="/login" replace />}
-          />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Suspense>
-      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <Login /> : <Navigate to="/profile" replace />}
+        />
+        <Route
+          path="/signup"
+          element={!isAuthenticated ? <Signup /> : <Navigate to="/profile" replace />}
+        />
+        <Route
+          path="/profile"
+          element={isAuthenticated ? <ProfilePage /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/update-profile"
+          element={isAuthenticated ? <UpdateProfile /> : <Navigate to="/login" replace />}
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
+        {/* <Footer /> */}
+        <ToastContainer />
       <Toaster position="bottom-right" />
     </>
   );
